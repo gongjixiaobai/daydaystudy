2015beb070458c33fd7946ab4bd6151a2deb524a
weiming02 <weiming02@megvii.com>
2021-09-16 11:46:29 +0800

    feat:build

---
+++ src/main/java/com/gongjixiaobai/GongjixiaobaiApplication.java
@@ -0,0 +1,11 @@
+package com.gongjixiaobai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GongjixiaobaiApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GongjixiaobaiApplication.class, args);
+    }
+}
